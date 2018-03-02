cb1d583363917ada529aa6b43685c15b4bad8f11
snkoski <shawnnkoski@gmail.com>
2018-03-02 03:28:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function theBeatlesPlay(array1, array2) {
   var theBand = [];
   
   var string1 = " plays ";
-  var string2;
   for(var i = 0; i < array1.length; i++) {
     theBand.push(array1[i] + string1 + array2[i]);
   }
