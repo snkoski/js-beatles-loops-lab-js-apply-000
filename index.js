b5573e2d9bf894d250c2aea65d21a484f02c8de6
snkoski <shawnnkoski@gmail.com>
2018-03-02 03:20:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,11 +40,11 @@ var num2 = 7;
 function iLoveTheBeatles(int) {
   var array = [];
   var string = "I love the Beatles!";
-  var string2 = "";
+  
   do  {
-    string2 += string;
     
-    array.push(string2);
+    
+    array.;
     num++;
   } while(num < 15)
   return array;
