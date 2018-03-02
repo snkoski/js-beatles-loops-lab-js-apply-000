bffc88fa6406795d4760236b78321920f666aa67
snkoski <shawnnkoski@gmail.com>
2018-03-02 01:47:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function iLoveTheBeatles(num) {
+  
+  var array = [];
+  var string = "I love the Beatles!";
+  
+  while (num < 15) {
+    array.push(string);
+    num++;
+  }
+  return array;
+}
+
+iLoveTheBeatles(5);
