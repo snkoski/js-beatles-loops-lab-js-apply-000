09363fdac02041abca3082a80b95fd777ea22386
snkoski <shawnnkoski@gmail.com>
2018-03-02 02:54:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var num = 7;
+var musicians = ["John Lennon", "Paul", "George", "Ringo"];
+var instruments = ["Guitar", "Bass", "Guitar", "Drums"];
 
 
-function iLoveTheBeatles(int) {
-  var array = [];
-  do  {
-    array.push("I love the Beatles!");
-    num++;
-  } while(num < 15)
-  return array;
+function theBeatlesPlay(array1, array2) {
+  var theBand = [];
+  
+  var string1 = " plays ";
+  var string2;
+  for(var i = 0; i < array1.length; i++) {
+    theBand.push(array1[i] + string1 + array2[i]);
+  }
+  console.log(theBand)
+  
 }
 
-iLoveTheBeatles(num);
+theBeatlesPlay(musicians, instruments);
